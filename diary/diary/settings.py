91aa58b12c7d1ac31f8bb1a1b63afb1e133dbe5c
nagesh <npknagesh@gmail.com>
2023-02-08 13:48:26 +0530

    revert the settings

--- diary/diary/settings.py
+++ diary/diary/settings.py
@@ -11,8 +11,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-import os
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-!^7!m)o764t7=8*gp+^l9fac(sxa0jkjaf(9v^*lx)+t=7lv!c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
