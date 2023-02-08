9fc9a5ffdecc087b594d4c86e51666050b31b5a9
nagesh <npknagesh@gmail.com>
2023-02-08 13:40:48 +0530

    modified settings and fixed template

--- diary/diary/settings.py
+++ diary/diary/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!^7!m)o764t7=8*gp+^l9fac(sxa0jkjaf(9v^*lx)+t=7lv!c'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
