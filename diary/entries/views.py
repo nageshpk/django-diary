9f1d608ef43151d4723e79eaa0057ab074a2a43e
nagesh <npknagesh@gmail.com>
2023-02-09 01:09:19 +0530

    add login

--- diary/entries/views.py
+++ diary/entries/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from .models import Enrty
 from django.contrib import messages
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 # Create your views here.
 
